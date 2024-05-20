16db0ce405922fb05e6ed6bc8331c3a275fae80c
pallavithorat <thoratpallavi2000@gmail.com>
2024-05-19 21:31:50 -0700

    Init

---
+++ src/home/Leftpart/User.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div>
+      <div className='flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer'>
+      <div className="avatar online">
+        <div className="w-12 rounded-full">
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+
+        <div>
+            <h1 className='font-bold'>Pallavi</h1>
+            <span>pallavi@gmail.com</span>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default User
---
+++ src/home/Rightpart/Right.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Chatuser from './Chatuser';
+import Messages from './Messages';
+import Typesend from './Typesend';
+
+function Right() {
+  return <div className=' w-[70%] bg-slate-900 text-gray-300'>
+    <Chatuser/>
+    <div className='flex-1 overflow-y-auto' style={{maxHeight:"calc(92vh - 8vh)"}}>
+        <Messages/>
+    </div>
+    <Typesend/>
+   </div>;
+  
+}
+
+export default Right;
